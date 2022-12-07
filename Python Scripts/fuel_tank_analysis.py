659f27055615efe5c5187a30803e0774ca7fb7ea
Hugo Bruins <hugobruins@gmail.com>
2022-12-07 15:12:53 +0100

    Create fuel_tank_analysis.py

---
+++ Python Scripts/fuel_tank_analysis.py
@@ -0,0 +1,39 @@
+class Material():
+    def __init__(self, E, sigma_y, alpha, v):
+        # Save all the properties
+        pass
+
+class Fueltank():
+    def __init__(self, p, volume, material):
+        # Get the mass
+        pass
+    
+
+class BeamAttachment():
+    def __init__(self, material):
+        # Calculate Ixx
+        
+        # Calculate spring constant lateral
+        
+        # Claculate spring constant axial
+        pass
+        
+class FuelTankAssembly():
+    def __init__(self, material):
+        pass
+    
+    def add_attachment(self):
+        pass
+    
+    def add_fuel_tank(self):
+        pass
+    
+    def get_natural_frequency(self):
+        pass
+
+    def get_total_mass(self):
+        pass\
+            
+hydrazine_fuel_tank = Fueltank()
+
+hydrazine_fuel_tank.mass
