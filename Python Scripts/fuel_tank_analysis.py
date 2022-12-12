0c4eb39fefefab32e3c4b2b1e3fb1e071b2d6b36
DionvanderArk <118368009+DionvanderArk@users.noreply.github.com>
2022-12-12 14:17:41 +0100

    Update fuel_tank_analysis.py

--- Python Scripts/fuel_tank_analysis.py
+++ Python Scripts/fuel_tank_analysis.py
@@ -1,6 +1,15 @@
 import math
 import numpy as np
 
+LF_Vol=0.362 #m^3
+Ox_Vol=0.359 #m^3
+SC_width=2 #m
+SC_depth=2 #m
+SC_height=3 #m
+LF_mass=303.2 #kg
+Ox_mass= 500.3 #kg
+tank_pressure=17E5 #Pa
+
 class Material():
     def __init__(self, E, sigma_y, density, v):
         # Save all the material properties in a container
@@ -12,6 +21,13 @@ class Material():
 class Fueltank():
     def __init__(self, p, volume, material):
         # Get the mass
+        self.radius=(3*V/(4*math.pi))**(1/3)
+        self.material=material
+        self.thickness=(p*self.radius*SF)/(2*self.material.sigma_y)
+        self.area=4*math.pi*self.radius**2
+        self.vol=self.area*self.thickness
+        self.mass=self.vol*self.material.density
+        self.mass_tot=self.mass+Fuel_mass
         pass
     
   
@@ -54,6 +70,9 @@ TI6ALV4  = Material(110e9, 825e6, 4400, 0.33)   # commonly used Titanium alloy
 MGA231B  = Material(45e9, 220e6, 1700, 0.33)    # magnesium alloy
 BES65A   = Material(304e9, 207e6, 2000, 0.33)   # berrylium alloy (very non toxic!)
 AM350    = Material(200e9, 1034e6, 7700, 0.33)  # ferrous alloy
+
+LF_tank=Fueltank(tank_pressure,LF_Vol,AL6061T6,1.5,LF_mass)
+Ox_tank=Fueltank(tank_pressure,Ox_Vol,AL6061T6,1.5,Ox_mass)
 
 # a beam
 square_beam = Beam(0.1, 1e-3, 1, AL6061T6)
